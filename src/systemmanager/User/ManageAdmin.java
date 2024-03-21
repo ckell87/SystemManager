ee93e15f0bf5c1ad7d832b1ed5aa9c40786ee231
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-21 19:31:56 +0000

    add new users
    sub menu for admin user to choose what type of new user they want to add.

--- src/systemmanager/User/ManageAdmin.java
+++ src/systemmanager/User/ManageAdmin.java
@@ -11,10 +11,11 @@ import java.util.Scanner;
  * @author under
  */
 public class ManageAdmin {
+
     public void adminManager() {
         // main method for admin user management
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Admin Manager Menu:");
         System.out.println("1. Add new user");
         System.out.println("2. Delete user");
@@ -30,12 +31,42 @@ public class ManageAdmin {
                 //  delete user
                 break;
             case 3:
-                // alter user details
+            // alter user details
             case 4:
                 System.out.println("Returning to main menu...");
                 break;
             default:
                 System.out.println("Invalid choice.");
         }
-}
+    }
+
+    public static void addNewUser() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please choose a number to add a new user");
+            System.out.println("1. Add admin user");
+            System.out.println("2. Add office user");
+            System.out.println("3. Add lecturer user");
+            System.out.println("4. Return to previous menu");
+
+            int choice = sc.nextInt();
+            sc.nextLine(); // Consume newline character before next user input
+
+            switch (choice) {
+                case 1:
+                    // Add admin user
+                    break;
+                case 2:
+                    // Add office user
+                    break;
+                case 3:
+                    // Add lecturer user
+                    break;
+                case 4:
+                    return; // Return to previous menu
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 }
