785880562ca8003683160252a357e33aa6dcb529
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-21 19:03:03 +0000

    main method
    method to main menu created in separate class. called upon in main method above.

--- src/systemmanager/SystemManager.java
+++ src/systemmanager/SystemManager.java
@@ -4,6 +4,8 @@
  */
 package systemmanager;
 
+import static systemmanager.MainSystemMenu.displayMainMenu;
+
 /**
  *
  * @author under
@@ -14,7 +16,7 @@ public class SystemManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        displayMainMenu();
     }
     
 }
