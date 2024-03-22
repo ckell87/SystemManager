bf084d6d58c3ade82a31560f41dc5399d9a95359
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-22 17:46:47 +0000

    change table format
    added first name and last name to mysql table

--- src/systemmanager/DatabaseConnection.java
+++ src/systemmanager/DatabaseConnection.java
@@ -6,8 +6,10 @@ package systemmanager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -18,17 +20,48 @@ public class DatabaseConnection {
     private static final String USER = "pooa2024";
     private static final String PASSWORD = "pooa2024";
     
-    public void newUserTable(String newUsers) throws SQLException{
+   /* public void newUserTable(String newUsers) throws SQLException{
        try {
            Connection conn = DriverManager.getConnection( DB_URL, USER, PASSWORD);
           Statement stmt = conn.createStatement();
           stmt.execute("USE CA2;");
-          stmt.execute("CREATE TABLE " + newUsers + " (username VARCHAR(25), password VARCHAR(25), role VARCHAR(25)); " );
+          stmt.execute("CREATE TABLE " + newUsers + " (first_name VARCHAR(25), last_name VARCHAR(25), username VARCHAR(25), password VARCHAR(25), role VARCHAR(25)); " );
            System.out.println("table sucessfully creted ");
            conn.close();
        } catch (Exception e) {
            e.printStackTrace();
        }
+
     }
+    */
+    public static void addNewUser() {
+    Scanner sc = new Scanner(System.in);
+     System.out.println("Enter first name ");
+    String first_name = sc.nextLine();
+    System.out.println("Enter last name ");
+    String last_name = sc.nextLine();
+    System.out.println("Enter username: ");
+    String username = sc.nextLine();
+    System.out.println("Enter password: ");
+    String password = sc.nextLine();
+    System.out.println("Enter role: ");
+    String role = sc.nextLine();
+    
+    // Insert new user into database
+    try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Statement stmt = conn.createStatement()) {
+            // Select the CA2 database
+            stmt.execute("USE CA2;");
+
+            // Insert new user into newUsers table
+            String query = "INSERT INTO newUsers (first_name, last_name, username, password, role) VALUES ('" + first_name + "', '" + last_name + "', '" + username + "', '" + password + "', '" + role + "')";
+            stmt.executeUpdate(query);
+
+            System.out.println("New user added successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error adding new user: " + e.getMessage());
+    }
+}
+
     
 }
