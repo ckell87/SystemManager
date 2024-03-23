252fc1b1f45e12b1e128f5cb3b00637edfb1bb85
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-23 16:39:35 +0000

    update username & password methods
    methods to authorise user to update their own username and password. re-used authenticateOfficeUser method.

--- src/systemmanager/User/ManageOffice.java
+++ src/systemmanager/User/ManageOffice.java
@@ -6,6 +6,7 @@ package systemmanager.User;
 
 import java.sql.SQLException;
 import java.util.Scanner;
+import systemmanager.DatabaseConnection;
 import static systemmanager.User.ManageAdmin.addNewUser;
 import static systemmanager.User.ManageAdmin.deleteUser;
 import static systemmanager.User.ManageAdmin.updateUser;
@@ -15,34 +16,76 @@ import static systemmanager.User.ManageAdmin.updateUser;
  * @author under
  */
 public class ManageOffice {
-    
+
     public void officeManager() throws SQLException {
         // main method for office user management
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Office Manager Menu:");
-        System.out.println("1. Change username ");
+        System.out.println("1. Change username");
         System.out.println("2. Change password");
         System.out.println("3. Generate Course report");
         System.out.println("4. Generate Student report");
-        System.out.println("3. Generate Lecturer report");
-        System.out.println("4. Back to main menu");
+        System.out.println("5. Generate Lecturer report");
+        System.out.println("6. Back to main menu");
         int choice = sc.nextInt();
+        sc.nextLine(); // consume newline character
 
         switch (choice) {
             case 1:
-                addNewUser();
+                changeUsername();
                 break;
             case 2:
-                deleteUser();
+                changePassword();
                 break;
             case 3:
-                updateUser();
+                //generateCourseReport();
+                break;
             case 4:
+                //generateStudentReport();
+                break;
+            case 5:
+                //generateLecturerReport();
+                break;
+            case 6:
                 System.out.println("Returning to main menu.");
                 break;
             default:
                 System.out.println("Invalid choice.");
+        }
+    }
+
+     public void changeUsername() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your username: ");
+        String username = sc.nextLine().trim();
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine().trim();
+        
+        if (DatabaseConnection.authenticateOfficeUser(username, password, "office")) { ////check user details before username change
+            System.out.print("Enter your new username: ");
+            String newUsername = sc.nextLine().trim();
+            
+             DatabaseConnection.updateOfficeUsername(username, newUsername);//// Update the username in the database
+            System.out.println("Username updated successfully.");
+        } else {
+            System.out.println("update failed. Incorrect username, password, or user role.");
+        }
+    }
+     public void changePassword() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your username: ");
+        String username = sc.nextLine().trim();
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine().trim();
+         if (DatabaseConnection.authenticateOfficeUser(username, password, "office")) { //check user details before username change
+            System.out.print("Enter your new password: ");
+            String newPassword = sc.nextLine().trim();
+            
+           DatabaseConnection.updateOfficePassword(username, newPassword); // Update the password in the database
+            System.out.println("Password updated successfully.");
+        } else {
+            System.out.println("Authentication failed. Incorrect username, password, or user role.");
         }
     }
 }
