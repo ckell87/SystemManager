0635d339f7f564e5a81f9d726e71d88e423954f3
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-23 12:03:58 +0000

    changed tables
    changed lecturer table name to staff to incorporate different users.
    newUsers table has been removed.
    new staff table now has 3 new columns.
    username, password and role

--- src/systemmanager/DatabaseConnection.java
+++ src/systemmanager/DatabaseConnection.java
@@ -22,20 +22,33 @@ public class DatabaseConnection {
     private static final String USER = "pooa2024";
     private static final String PASSWORD = "pooa2024";
 
-    /* public void newUserTable(String newUsers) throws SQLException{
-       try {
-           Connection conn = DriverManager.getConnection( DB_URL, USER, PASSWORD);
-          Statement stmt = conn.createStatement();
-          stmt.execute("USE CA2;");
-          stmt.execute("CREATE TABLE " + newUsers + " (first_name VARCHAR(25), last_name VARCHAR(25), username VARCHAR(25), password VARCHAR(25), role VARCHAR(25)); " );
-           System.out.println("table sucessfully creted ");
-           conn.close();
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+    public void alterTable() throws SQLException {
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();
+            stmt.execute("USE CA2;");
+            stmt.execute(" ALTER TABLE staff " + " ADD COLUMN username VARCHAR(25) AFTER lname, "
+                    + " ADD COLUMN password VARCHAR(25) AFTER username, "
+                    + " ADD COLUMN role VARCHAR(25) AFTER password; ");
+            System.out.println("column sucessfully created");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
-     */
+
+    public void renameTable() {
+
+        try ( Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);  Statement stmt = conn.createStatement()) {
+            stmt.execute("USE CA2;");
+            stmt.execute("RENAME TABLE  lecturer  TO  staff;");
+            System.out.println("Table renamed successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static boolean authenticateAdminUser(String username, String password, String role) throws SQLException { // Check if username, password and role match the input
         try ( Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
             try ( Statement stmt = conn.createStatement()) {
@@ -66,5 +79,4 @@ public class DatabaseConnection {
         }
     }
 
-    
 }
